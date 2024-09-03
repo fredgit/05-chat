f8e279598dcfa9625bdcbdf2f37ea525fd3bbbf7
Fred Chen <canfeng.chen917@gmail.com>
2024-09-04 07:36:08 +0800

    feature: support jwt for signin & signup api

--- chat_server/src/lib.rs
+++ chat_server/src/lib.rs
@@ -2,10 +2,14 @@ mod config;
 mod error;
 mod handlers;
 mod models;
+mod utils;
 
-use std::{ops::Deref, sync::Arc};
+use anyhow::Context;
+use std::{fmt, ops::Deref, sync::Arc};
+use utils::{DecodingKey, EncodingKey};
 
-pub use error::AppError;
+pub use error::{AppError, ErrorOutput};
+
 pub use models::User;
 
 use axum::{
@@ -14,6 +18,7 @@ use axum::{
 };
 pub use config::AppConfig;
 use handlers::*;
+use sqlx::PgPool;
 
 #[derive(Debug, Clone)]
 pub(crate) struct AppState {
@@ -21,13 +26,15 @@ pub(crate) struct AppState {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
 pub(crate) struct AppStateInner {
     pub(crate) config: AppConfig,
+    pub(crate) dk: DecodingKey,
+    pub(crate) ek: EncodingKey,
+    pub(crate) pool: PgPool,
 }
 
-pub fn get_router(config: AppConfig) -> Router {
-    let state = AppState::new(config);
+pub async fn get_router(config: AppConfig) -> Result<Router, AppError> {
+    let state = AppState::try_new(config).await?;
 
     let api = Router::new()
         .route("/signin", post(signin_handler))
@@ -41,10 +48,12 @@ pub fn get_router(config: AppConfig) -> Router {
         )
         .route("/chat/:id/messages", get(list_message_handler));
 
-    Router::new()
+    let app = Router::new()
         .route("/", get(index_handler))
         .nest("/api", api)
-        .with_state(state)
+        .with_state(state);
+
+    Ok(app)
 }
 
 // 当调用 state.config 其实是调用了 state.inner.config
@@ -57,9 +66,56 @@ impl Deref for AppState {
 }
 
 impl AppState {
-    pub fn new(config: AppConfig) -> Self {
-        Self {
-            inner: Arc::new(AppStateInner { config }),
-        }
+    pub async fn try_new(config: AppConfig) -> Result<Self, AppError> {
+        let dk = DecodingKey::load(&config.auth.pk).context("Failed to load decoding key")?;
+        let ek = EncodingKey::load(&config.auth.sk).context("Failed to load encoding key")?;
+        let pool = PgPool::connect(&config.server.db_url)
+            .await
+            .context("Failed to connect to database")?;
+
+        Ok(Self {
+            inner: Arc::new(AppStateInner {
+                config,
+                dk,
+                ek,
+                pool,
+            }),
+        })
+    }
+}
+
+impl fmt::Debug for AppStateInner {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("AppStateInner")
+            .field("config", &self.config)
+            .finish()
+    }
+}
+
+#[cfg(test)]
+impl AppState {
+    pub async fn new_for_test(
+        config: AppConfig,
+    ) -> Result<(sqlx_db_tester::TestPg, Self), AppError> {
+        use sqlx_db_tester::TestPg;
+        let dk = DecodingKey::load(&config.auth.pk).context("load pk failed")?;
+        let ek = EncodingKey::load(&config.auth.sk).context("load sk failed")?;
+        let post = config.server.db_url.rfind('/').expect("invalid db_url");
+        let server_url = &config.server.db_url[..post];
+
+        let tdb = TestPg::new(
+            server_url.to_string(),
+            std::path::Path::new("../migrations"),
+        );
+        let pool = tdb.get_pool().await;
+        let state = Self {
+            inner: Arc::new(AppStateInner {
+                config,
+                ek,
+                dk,
+                pool,
+            }),
+        };
+        Ok((tdb, state))
     }
 }
